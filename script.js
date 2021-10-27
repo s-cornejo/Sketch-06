5d0a2b043a0052cd583a55da68083022bf0bd58c
s-cornejo <scornejo@conncoll.edu>
2021-10-27 12:16:11 -0400

    updated code

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 $(document).ready(function() {
 
-    var nounArr = ["Santa Claus", "Doja Cat", "the president", "dog", "bird"];
+    var nounArr = ["Santa Claus", "Doja Cat", "the president", "a dog", "a bird"];
 
-    var verbArr = ["run", "cough", "fall", "sit", "drink"];
+    var verbArr = ["barely run", "cough uncontrollably", "fall", "take a nap", "sip some tea"];
 
     var adjArr = ["beautiful", "slimy", "trash", "hilarious", "boring"];
+
+    var placeArr = ["Saturn", "Mickey Ds", "Austin, Texas"];
 
     $("button").click(function() {
         $(".noun").each(function() {
             var nounRand = Math.floor(Math.random() * nounArr.length);
             $(this).text(nounArr[nounRand]);
-        });
-
-        // $(".verb").each(function() {
-        //     var verbRand = Math.floor(Math.random() * verbArr.length);
-        //     $(this).text(verbArr[verbRand]);
-        // });
-
-        // $(".adj").each(function() {
-        //     var adjRand = Math.floor(Math.random() * adjArr.length);
-        //     $(this).text(adjArr[adjRand]);
-        // });   
+        });  
     });
 
     $("button").click(function() {
@@ -35,5 +27,12 @@ $(document).ready(function() {
             var adjRand = Math.floor(Math.random() * adjArr.length);
             $(this).text(adjArr[adjRand]);
         });  
+    });
+
+    $("button").click(function() {
+        $(".verb").each(function() {
+            var placeRand = Math.floor(Math.random() * placeArr.length);
+            $(this).text(placeArr[placeRand]);
+        });
     });
 });
