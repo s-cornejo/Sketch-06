6fdf7c6dcdf780300e1c160c9b0fc91e81356393
s-cornejo <scornejo@conncoll.edu>
2021-10-27 11:31:10 -0400

    updated code

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ $(document).ready(function() {
             $(this).text(nounArr[nounRand]);
         });
 
-        $(".verb").click(function() {
+        $(".verb").each(function() {
             var verbRand = Math.floor(Math.random() * verbArr.length);
             $(this).text(verbArr[verbRand]);
         });
 
-        $(".adj").click(function() {
+        $(".adj").each(function() {
             var adjRand = Math.floor(Math.random() * adjArr.length);
             $(this).text(adjArr[adjRand]);
         });
